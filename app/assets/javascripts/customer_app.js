b006c248f4262f7f52c414ec80084f01be8f49a2
Phan Thai Phong <fongfan999@gmail.com>
2016-07-27 18:45:27 +0700

    Fix pagination

--- app/assets/javascripts/customer_app.js
+++ app/assets/javascripts/customer_app.js
@@ -1,16 +1,32 @@
 var app = angular.module("customers", [])
 
 var CustomerSearchControl = function($scope, $http) {
-  $scope.customers = []
+  var page = 0;
   $scope.search = function(searchTerm) {
     $http.get("/customers.json",
-      { "params": { "keywords": searchTerm } }
+      { "params": { "keywords": searchTerm, "page": page } }
     ).then(function(response) {
-      $scope.customers = response.data
+      $scope.customers = response.data;
+      if (page == 0) {
+        angular.element(".previous").addClass("disabled");
+      };
     }, function(response) {
-      alert("There was a problem: " + response.status)
+      alert("There was a problem: " + response.status);
     });
-  }
+  };
+
+  $scope.previousPage = function() {
+    if (page > 0) {
+      page = page - 1;
+      $scope.search($scope.keywords);
+    };
+  };
+
+  $scope.nextPage = function() {
+    angular.element(".previous").removeClass("disabled");
+    page = page + 1;
+    $scope.search($scope.keywords);
+  };
 }
 
 app.controller("CustomerSearchControl",
